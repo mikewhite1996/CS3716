2599d7114540ce7139009fb55cb013ffb34e40d0
Matt Gilchrist <mag@s1020n14h177.wst1.mun.ca>
2017-11-30 21:52:29 -0330

    turns the big sting into smaller ones to make into drop down options

---
+++ Loop.java
@@ -0,0 +1,170 @@
+public class Loop {
+public static void main(String[] args) {
+
+
+  
+
+  String s1 = "";
+  String s2 = "";
+  String s3 = "";
+  String s4 = "";
+  String s5 = "";
+  String s6 = "";
+  String s7 = "";
+  String s8 = "";
+  String s9 = "";
+  String s10 = "";
+  String s11 = "";
+  String s12 = "";
+  String s13 = "";
+
+
+
+if (s.length() == 14) {
+  s1 = s.substring(0,11);
+
+  }
+if (s.length() == 21) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  }
+
+if (s.length() == 28) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+
+}
+if (s.length() == 35) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+
+
+}
+if (s.length() == 42) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+
+
+}
+if (s.length() == 49) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+
+
+}
+if (s.length() == 56) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+
+
+}
+if (s.length() == 63) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+  s8 = s.substring(49, 60);
+
+
+}
+if (s.length() == 70) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+  s8 = s.substring(49, 60);
+  s9 = s.substring(56, 67);
+
+
+
+}
+if (s.length() == 77) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+  s8 = s.substring(49, 60);
+  s9 = s.substring(56, 67);
+  s10 = s.substring(63, 74);
+
+
+
+
+}
+if (s.length() == 84) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+  s8 = s.substring(49, 60);
+  s9 = s.substring(56, 67);
+  s10 = s.substring(63, 74);
+  s11 = s.substring(70, 81);
+
+
+
+}
+if (s.length() == 91) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+  s8 = s.substring(49, 60);
+  s9 = s.substring(56, 67);
+  s10 = s.substring(63, 74);
+  s11 = s.substring(70, 81);
+  s12 = s.substring(77, 88);
+
+
+
+}
+if (s.length() == 98) {
+  s1 = s.substring(0,11);
+  s2 = s.substring(7,18);
+  s3 = s.substring(14, 25);
+  s4 = s.substring(21, 32);
+  s5 = s.substring(28, 39);
+  s6 = s.substring(35, 46);
+  s7 = s.substring(42, 53);
+  s8 = s.substring(49, 60);
+  s9 = s.substring(56, 67);
+  s10 = s.substring(63, 74);
+  s11 = s.substring(70, 81);
+  s12 = s.substring(77, 88);
+  s13 = s.substring(84, 95);
+
+
+}
+
+}
+}
